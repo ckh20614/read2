b432c37b40729b69de5420515c550d0b9dbeef46
kai han <kevin061415@gmail.com>
2021-06-03 12:57:49 +0800

    word_count

--- data2.py
+++ data2.py
@@ -8,9 +8,26 @@ with open('reviews.txt', 'r') as f:
 			print(len(data))
 print('資料讀取完了，總共有', len(data), '筆資料')
 
-print(data[0])
+
+sum_led = 0
+for d in data:
+	sum_led = sum_led + len(d)
+print('資料平均長度為', sum_led/len(data))
+
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print('一共有', len(new), '筆資料長度小於100')
+
+good = []
+for d in data:
+	if 'good' in d:
+		good.append(d)
+print('一共有', len(good), '筆資料有提到good')
 
 
+# 文字記數
 wc ={}
 for d in data:
 	words = d.split(' ')
@@ -18,7 +35,7 @@ for d in data:
 		if word in wc:
 			wc[word] += 1
 		else:
-			wc[word] = 1 
+			wc[word] = 1  #新增新的key至wc字典
 
 for word in wc:
 	if wc[word] > 1000000:
@@ -35,37 +52,3 @@ while True:
 		print(name, '這個字出現過', wc[name], '次')
 	else:
 		print('沒有這個字喔!')
-
-	
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# sum_led = 0
-# for d in data:
-# 	sum_led = sum_led + len(d)
-# print('資料平均長度為', sum_led/len(data))
-
-# new = []
-# for d in data:
-# 	if len(d) < 100:
-# 		new.append(d)
-# print('一共有', len(new), '筆資料長度小於100')
-
-# good = []
-# for d in data:
-# 	if 'good' in d:
-# 		good.append(d)
-# print('一共有', len(good), '筆資料有提到good')
